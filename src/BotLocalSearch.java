affbd1927c34f8e8e03ea4a44e68975b0a81def7
Jimly-Firdaus <13521102@std.stei.itb.ac.id>
2023-10-15 16:25:55 +0700

    feat: simulated annealing implementation
    Co-authored-by: Bill Clinton <summerrs528@gmail.com>

--- src/BotLocalSearch.java
+++ src/BotLocalSearch.java
@@ -1,13 +1,40 @@
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Random;
+
 public class BotLocalSearch implements Bot {
-    
-    // private int[][] gameState;
+    private char botChar;
     private double temperature;
+
+    // TODO: change this to appropriate value
+    private final double MOVE_THRESHOLD_PROBABILITY = 0.1;
+    private final double TEMPERATURE_RATE_DECREASE = 0.1;
+
+    public BotLocalSearch(char givenChar) {
+        this.botChar = givenChar;
+
+        // TODO: change this to appropriate value
+        this.temperature = 2.0;
+    }
 
     private double moveAcceptanceProbability(double currentScore, double newScore, double temperature) {
         if (newScore > currentScore) {
             return 1.0;
         }
         return Math.exp((newScore - currentScore) / temperature);
+    }
+
+    private int evaluate(char[][] checkedBoard, char checkChar) {
+        int score = 0;
+        for (int i = 0; i < MAX_ROW_MOVEMENT_ALLOWED; i++) {
+            for (int j = 0; j < MAX_COL_MOVEMENT_ALLOWED; j++) {
+                if (checkChar == checkedBoard[i][j]) {
+                    score++;
+                }
+            }
+        }
+
+        return score;
     }
 
     public int[] move(char[][] board) {
@@ -20,12 +47,53 @@ public class BotLocalSearch implements Bot {
             }
             System.out.println("\n---------------------------------");
         }
+        System.out.println();
 
-        return new int[]{(int) (Math.random()*8), (int) (Math.random()*8)};
+        int currentScore = this.evaluate(board, this.botChar);
+        List<int[]> successors = new ArrayList<int[]>();
+
+        // invoke all successsors
+        for (int i = 0; i < MAX_ROW_MOVEMENT_ALLOWED; i++) {
+            for (int j = 0; j < MAX_COL_MOVEMENT_ALLOWED; j++) {
+                if (isEmptyCoordinate(i, j, board)) {
+                    successors.add(new int[] { i, j });
+                }
+            }
+        }
+
+        Random rand = new Random();
+
+        while (this.temperature > 0) {
+            int randomPoint = rand.nextInt(successors.size());
+            char[][] modifiedBoard = this.patchNewChar(successors.get(randomPoint)[0], successors.get(randomPoint)[1],
+                    board, this.botChar);
+
+            int evaluation = this.evaluate(modifiedBoard, this.botChar);
+
+            // TODO: change this to appropriate value
+            this.temperature -= this.TEMPERATURE_RATE_DECREASE;
+
+            // if found good neighbor
+            if (evaluation > currentScore)
+                return successors.get(randomPoint);
+
+            // if found bad neighbor
+            if (this.moveAcceptanceProbability(currentScore, evaluation,
+                    this.temperature) > this.MOVE_THRESHOLD_PROBABILITY) {
+                return successors.get(randomPoint);
+            }
+        }
+
+        int randomPoint = rand.nextInt(successors.size());
+        return successors.get(randomPoint);
     }
 
-    public boolean isEmptyCoordinate(int i, int j, char[][] board) {
+    private boolean isEmptyCoordinate(int i, int j, char[][] board) {
         return (board[i][j] == ' ');
     }
 
+    private char[][] patchNewChar(int i, int j, char[][] board, char character) {
+        board[i][j] = character;
+        return board;
+    }
 }
