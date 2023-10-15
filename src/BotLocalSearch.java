d385d25e34ceea6dc020ad2e3b312556715ad8a0
billc27 <110593711+billc27@users.noreply.github.com>
2023-10-15 14:33:08 +0700

    refactor: edit testing code

--- src/BotLocalSearch.java
+++ src/BotLocalSearch.java
@@ -7,11 +7,13 @@ public class BotLocalSearch implements Bot {
 
         for (int i = 0; i < MAX_ROW_MOVEMENT_ALLOWED; i++) {
             for (int j = 0; j < MAX_COL_MOVEMENT_ALLOWED; j++) {
-                System.out.print(board[i][j] + " - ");
+                if (j == 0) {
+                    System.out.print("| ");
+                }
+                System.out.print(board[i][j] + " | ");
             }
-            System.out.println("");
+            System.out.println("\n---------------------------------");
         }
-
 
         return new int[]{(int) (Math.random()*8), (int) (Math.random()*8)};
         // return null;
