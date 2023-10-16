0453f7797ecd1b43076dd1652c01f5faa03e5ac9
yuujin-Q <13521074@mahasiswa.itb.ac.id>
2023-10-16 19:33:53 +0700

    feat: priority queue for minmax call

--- src/BotMinimaxAlphaBethaPruning.java
+++ src/BotMinimaxAlphaBethaPruning.java
@@ -1,3 +1,5 @@
+import java.util.PriorityQueue;
+
 public class BotMinimaxAlphaBethaPruning implements Bot {
     private char botChar;
     private char nonBotChar;
@@ -10,27 +12,84 @@ public class BotMinimaxAlphaBethaPruning implements Bot {
         this.nonBotChar = nonBotChar;
     }
 
+    public static class NodeQueueElement implements Comparable<NodeQueueElement> {
+        private int priority;
+        public char[][] board;
+        public int depth;
+        public boolean isMaximizing;
+        public int alpha;
+        public int beta;
+        public int i;
+        public int j;
+
+
+        public NodeQueueElement(int priority, char[][] board, int depth, boolean isMaximizing, int alpha, int beta, int i, int j) {
+            this.priority = priority;
+            this.board = board;
+            this.depth = depth;
+            this.isMaximizing = isMaximizing;
+            this.alpha = alpha;
+            this.beta = beta;
+            this.i = i;
+            this.j = j;
+        }
+
+        @Override
+        public int compareTo(NodeQueueElement o) {
+            if (this.priority < o.priority) {
+                return 1;
+            } else if (this.priority == o.priority) {
+                return 0;
+            } else {
+                return -1;
+            }
+        }
+    }
+
     private boolean emptyNeighbor(int i, int j, char[][] board){
         boolean isEmpty = true;
         
         if (i != 0) {
             isEmpty &= isEmptyCoordinate(i - 1, j, board);
-            // isempty &=bawah
-        } 
+        }
         if (i != MAX_ROW_MOVEMENT_ALLOWED - 1) {
-            // isemtpy &= atas
             isEmpty &= isEmptyCoordinate(i + 1, j, board);
         }
         if (j != 0) {
-            // &= kanan
             isEmpty &= isEmptyCoordinate(i, j - 1, board);
         } 
         if (j != MAX_COL_MOVEMENT_ALLOWED - 1) {
-            //  &=kiri
             isEmpty &= isEmptyCoordinate(i, j + 1, board);
         }
 
         return isEmpty;
+    }
+
+    private int countSurroundingEnemyTile(int i, int j, char[][] board) {
+        int surroundingEnemyCount = 0;
+
+        if (i != 0) {
+            if (board[i - 1][j] == nonBotChar) {
+                surroundingEnemyCount++;
+            }
+        }
+        if (i != MAX_ROW_MOVEMENT_ALLOWED - 1) {
+            if (board[i + 1][j] == nonBotChar) {
+                surroundingEnemyCount++;
+            }
+        }
+        if (j != 0) {
+            if (board[i][j - 1] == nonBotChar) {
+                surroundingEnemyCount++;
+            }
+        }
+        if (j != MAX_COL_MOVEMENT_ALLOWED - 1) {
+            if (board[i][j + 1] == nonBotChar) {
+                surroundingEnemyCount++;
+            }
+        }
+
+        return surroundingEnemyCount;
     }
 
     private Tuple<Integer, Integer> bestMove(char[][] checkBoard){
@@ -39,30 +98,60 @@ public class BotMinimaxAlphaBethaPruning implements Bot {
         int alpha = Integer.MIN_VALUE;
         int beta = Integer.MAX_VALUE;
         Tuple<Integer, Integer> move = new Tuple<>(-1,-1);
-        boolean prunning = false;
+//        boolean prunning = false;
+
+        PriorityQueue<NodeQueueElement> pq = new PriorityQueue<>();
         for (int i= 0; i < MAX_ROW_MOVEMENT_ALLOWED; i++){
             for(int j= 0; j<MAX_COL_MOVEMENT_ALLOWED; j++){
                 if(isEmptyCoordinate(i, j, checkBoard) && !emptyNeighbor(i, j, checkBoard)){
                     checkBoard[i][j] = this.botChar;
-                    score = this.minimax(checkBoard, 0, false, alpha, beta);
-                    checkBoard[i][j] = ' ';
-                    if(score > bestScore){
-                        bestScore = score;
-                        alpha = Math.max(alpha, score);
-                        if(beta <= alpha){
-                            prunning = true;
-                            break;
-                        }else{
-                            move.setFirst(i);
-                            move.setSecond(j);
-                        }
-                    }
+
+//                    cek tile lawan
+
+
+                    pq.add(new NodeQueueElement(countSurroundingEnemyTile(i, j, checkBoard), checkBoard, 0, false, alpha, beta, i, j));
+
+//                    enqueue
+//                    score = this.minimax(checkBoard, 0, false, alpha, beta);
+//
+//                    checkBoard[i][j] = ' ';
+//                    if(score > bestScore){
+//                        bestScore = score;
+//                        alpha = Math.max(alpha, score);
+//                        if(beta <= alpha){
+//                            prunning = true;
+//                            break;
+//                        }else{
+//                            move.setFirst(i);
+//                            move.setSecond(j);
+//                        }
+//                    }
                 }
             }
-            if(prunning){
-                break;
+//            if(prunning){
+//                break;
+//            }
+        }
+//        pop queue disini
+        while (!pq.isEmpty()) {
+            NodeQueueElement n = pq.poll();
+
+            score = this.minimax(n.board, n.depth, n.isMaximizing, n.alpha, n.beta);
+
+            checkBoard[n.i][n.j] = ' ';
+            if(score > bestScore){
+                bestScore = score;
+                alpha = Math.max(alpha, score);
+                if(beta <= alpha){
+                    pq.clear();
+                    break;
+                }else{
+                    move.setFirst(n.i);
+                    move.setSecond(n.j);
+                }
             }
         }
+
         return move;
     }
 
